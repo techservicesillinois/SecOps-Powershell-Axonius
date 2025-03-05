e4e8b798f48522faf15a7d0983d5fce00122239e
Tamara Buch <buch1@illinois.edu>
2025-03-05 11:59:59 -0600

    changelog++ (#1)
    * changelog++
    
    * Pester Tests fix--add env var

--- test/UofIAxonius.Tests.ps1
+++ test/UofIAxonius.Tests.ps1
@@ -1,5 +1,9 @@
-[String]$ModuleRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src\UofIAxonius'
-Import-Module -Name $ModuleRoot
+# Need the TDXSettings environment variable set to be able to import the module and run the tests
+BeforeAll{
+    $env:AxoniusSettings = '{ "BaseURI": [""] }'
+    [String]$ModuleRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src\UofIAxonius'
+    Import-Module -Name $ModuleRoot
+}
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
