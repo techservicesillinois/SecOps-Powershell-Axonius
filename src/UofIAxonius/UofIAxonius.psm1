4fbca2dac0cef28b4135ab9909af9f6ba2f015d0
Tamara Buch <buch1@illinois.edu>
2025-03-05 11:10:04 -0600

    settings++

--- src/UofIAxonius/UofIAxonius.psm1
+++ src/UofIAxonius/UofIAxonius.psm1
@@ -1,3 +1,16 @@
+# Use environment variable for TDXSettings (explained in README.md)
+if ($env:AxoniusSettings) {
+    $script:Settings=$env:AxoniusSettings | ConvertFrom-Json
+}
+else {
+    Write-Error -Message "No environment variable AxoniusSettings found.
+    Set `$env:AxoniusSettings to a JSON-formatted string containing a BaseURI property, for example:
+    `$env:AxoniusSettings='{ ""BaseURI"": [""https://{axonius-URI}:{port}/api/""] }'"
+}
+
+$Script:Session = $NULL
+[int]$Script:APICallCount = 0
+
 [String]$FunctionPath = Join-Path -Path $PSScriptRoot -ChildPath 'Functions'
 #All function files are executed while only public functions are exported to the shell.
 Get-ChildItem -Path $FunctionPath -Filter "*.ps1" -Recurse | ForEach-Object -Process {
