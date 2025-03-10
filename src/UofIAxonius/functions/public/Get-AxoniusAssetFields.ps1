8b4bc99eb3cceb1c96c7ed35aec00dfee4346f54
Tamara Buch <buch1@illinois.edu>
2025-03-10 16:27:25 -0500

    Get-AxoniusAssetFields

--- src/UofIAxonius/functions/public/Get-AxoniusAssetFields.ps1
+++ src/UofIAxonius/functions/public/Get-AxoniusAssetFields.ps1
@@ -1,69 +1,47 @@
 <#
 .Synopsis
-    Get details of all saved queries.
+    Get all available fields for the provided asset type.
 .DESCRIPTION
-    Get details of all saved queries.
-.PARAMETER FolderID
-    The ID of the folder to pull from. Use all to pull from all folders.
-    Defaults to 'all'.
-.PARAMETER UsedIn
-    A list of system modules where the queries are used
-.PARAMETER IncludeMetadata
-    When true, returns metadata such as item count and paging information (page number, offset, limit). For use when returning large amounts of data
-    Defaults to 'true'.
-.PARAMETER Offset
-    The number of rows to skip from the beginning of the result set
-.PARAMETER Limit
-    The number of rows to return from the result set
-    Defaults to 1000.
+    Get all available fields for the provided asset type.
+.PARAMETER AssetType
+    Retrieve assets for the selected asset type. This is required.
+.PARAMETER Search
+    Search term to filter by. The response will include all the fields that contain the search term.
+    For example, if the search term is network, all fields that contain network will be returned
+.PARAMETER ExcludeSubfields
+    If set to true, the request will return all the fields except subfields.
+    For example, the field specific_data.data.network_interfaces will be included in the response but specific_data.data.network_interfaces.ips_preferred will be excluded.
 .EXAMPLE
-    Get-AxoniusQueries
+    Get-AxoniusAssetFields -AssetType 'devices'
 .EXAMPLE
-    Get-AxoniusQueries -FolderID 'Public' -UsedIn @('dashboard','enforcements') -IncludeMetadata -Offset 100 -Limit 100
+    Get-AxoniusAssetFields -AssetType 'devices' -Search 'network' -ExcludeSubfields
 #>
 function Get-AxoniusAssetFields{
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '',
             Justification = 'This is consistent with the vendors verbiage')]
     param (
-        [Alias('folder_id')]
-        [string]$FolderID = 'all',
-        [Alias('used_in')]
-        [string[]]$UsedIn,
-        [Alias('include_metadata')]
-        [switch]$IncludeMetadata,
-        [int]$Offset,
-        [int]$Limit
+        [Parameter(Mandatory=$true)]
+        [string]$AssetType,
+        [string]$Search,
+        [Alias('exclude_subfields')]
+        [switch]$ExcludeSubfields
     )
 
     process{
 
         $QueryObjects = @()
 
-        # Always add folder_id, even if it's the default or explicitly set
-        $QueryObjects += "folder_id=$FolderID"
-
-        If(-not $IncludeMetadata){
-            $QueryObjects += "&include_metadata=false"
-        }
-
         $PSCmdlet.MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object {
-            $alias = $MyInvocation.MyCommand.Parameters[$_.Key].Aliases[0]
-            $paramName = $alias ?? $_.Key
-
-            # Handle array parameters by adding multiple entries
-            if ($_.Value -is [array]) {
-                foreach ($value in $_.Value) {
-                    $QueryObjects += "$($paramName)=$value"
-                }
-            }
-            else {
+            if($_.Key -notlike 'AssetType'){
+                $alias = $MyInvocation.MyCommand.Parameters[$_.Key].Aliases[0]
+                $paramName = $alias ?? ($_.Key).ToLower()
                 $QueryObjects += "$($paramName)=$($_.Value)"
             }
         }
 
         $QueryString = $QueryObjects -join "&"
-        $RelativeUri = "queries?$($QueryString)"
+        $RelativeUri = "assets/$($AssetType)/fields?$($QueryString)"
 
         $RestSplat = @{
             Method      = 'GET'
